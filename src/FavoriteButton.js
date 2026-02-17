b2341766e5b43c769b42f42c3806c0f4b7eeef73
ricardo <rfigueroa2317@gmail.com>
2026-02-17 18:29:45 -0500

    adding shaking animation

--- src/FavoriteButton.js
+++ src/FavoriteButton.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Heart } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import "./FavoriteButton.scss";
 
 const FavoriteButton = ({ cocktail, favorites, onToggle }) => {
+  const navigate = useNavigate();
+  const [isShaking, setIsShaking] = useState(false);
   const isFavorite = favorites?.some((fav) => fav.id === cocktail.id);
+  const isLoggedIn = false;
 
   const handleToggle = (e) => {
     e.preventDefault();
     e.stopPropagation();
+
+    if (!isLoggedIn) {
+      setIsShaking(true);
+      setTimeout(() => {
+        setIsShaking(false);
+
+        const wantToJoin = window.confirm(
+          "Want to save your favorite drinks? Sign up for a Vivid Pour account!",
+        );
+
+        if (wantToJoin) {
+          navigate("/login", { state: { startInSignUp: true } });
+        }
+      }, 400);
+
+      return;
+    }
     onToggle(cocktail);
   };
 
   return (
     <button
       onClick={handleToggle}
-      className={`favorite-btn ${isFavorite ? "active" : ""}`}
+      className={`favorite-btn ${isFavorite ? "active" : ""} ${isShaking ? "error-shake" : ""}`}
       aria-label="Toggle Favorite"
     >
       <Heart
         size={20}
         className="heart-icon"
         fill={isFavorite ? "#ff007f" : "none"}
+        color={isFavorite ? "#ff007f" : "#00ced1"}
       />
     </button>
   );
